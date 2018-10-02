f7eb2e1050e0a223d6c1adbb0993ec037b1bdaf3
Timothy <superfluous11@gmail.com>
2018-10-02 08:50:13 -0700

    Completed calculator drill

--- rate-calculator2/src/components/number-input.js
+++ rate-calculator2/src/components/number-input.js
@@ -2,9 +2,10 @@ import React from 'react';
 
 export default function NumberInput(props) {
     return (
+
         <div className="form-group">
             <label htmlFor={props.id}>{props.label}</label>
-            <input type="number" id={props.id} min={props.min} max={props.max} />
+            <input type="number" id={props.id} min={props.min} max={props.max} value={props.value} onChange={e => props.updateValue(e.target.value)}/>
         </div>
     );
 }
--- rate-calculator2/src/components/rate-calculator.js
+++ rate-calculator2/src/components/rate-calculator.js
@@ -6,16 +6,20 @@ import Output from './output';
 export default class RateCalculator extends React.Component {
     constructor(props) {
         super(props);
+        this.state = {
+          dayrate: 300,
+          hours: 5
+        }
     }
 
     render() {
-        const rate = 10;
+        const rate = parseFloat(this.state.dayrate) / parseFloat(this.state.hours);
+
         return (
             <form>
-                <NumberInput id="day-rate" label="Day rate" min={0} max={5000} />
-                <NumberInput id="hours" label="Hours" min={1} max={12} />
-                <Output id="hourly-rate" label="Hourly rate" value={rate.toFixed(2)}
-/>
+                <NumberInput id="day-rate" updateValue={rate=> this.setState({dayrate: rate})} value={this.state.dayrate} label="Day rate" min={0} max={5000} />
+                <NumberInput id="hours" updateValue={hours => this.setState({ hours })} value={this.state.hours} label="Hours" min={1} max={12} />
+                <Output id="hourly-rate" label="Hourly rate" value={rate.toFixed(2)}/>
             </form>
         );
     }
