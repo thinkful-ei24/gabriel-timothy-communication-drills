a62ca170977bd22f47515470f23b7538bc6f0687
Gabriel Morris <gabemorris@outlook.com>
2018-10-02 08:27:59 -0700

    added surprise app

--- surprise/src/components/surprise-button.js
+++ surprise/src/components/surprise-button.js
@@ -1,6 +1,5 @@
 import React from 'react';
 
 export default function SurpriseButton(props) {
-    return <button>Surprise!</button>;
+  return <button onClick={props.showImage}>Surprise!</button>;
 }
-
--- surprise/src/components/surprise.js
+++ surprise/src/components/surprise.js
@@ -4,12 +4,25 @@ import SurpriseButton from './surprise-button';
 import SurpriseImage from './surprise-image';
 
 export default class Surprise extends React.Component {
-    constructor(props) {
-        super(props);
-    }
+  constructor(props) {
+    super(props);
 
-    render() {
-        // Show the button to start with
-        return <SurpriseButton />;
+    this.state = { imageIsShown: false };
+  }
+
+  render() {
+    // Show the button to start with
+    if (this.state.imageIsShown) {
+      return <SurpriseImage />;
+    } else {
+      return (
+        <SurpriseButton
+          showImage={() => {
+            console.log('button clicked');
+            this.setState({ imageIsShown: true });
+          }}
+        />
+      );
     }
+  }
 }
